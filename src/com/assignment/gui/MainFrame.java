7caccb5a9df4d014ec7f0f693b397e360a150cd6
Sherry Ma <sherry.hyma@gmail.com>
2018-11-06 13:24:48 -0500

    insert gui done

--- src/com/assignment/gui/MainFrame.java
+++ src/com/assignment/gui/MainFrame.java
@@ -42,6 +42,21 @@ public class MainFrame {
 	private JTextField textField_8;
 	private JTextField textField_9;
 	private JTextField textField_11;
+	private JTextField textField_16;
+	private JTextField textField_17;
+	private JTextField textField_18;
+	private JTextField textField_19;
+	private JTextField textField_20;
+	private JTextField textField_21;
+	private JTextField textField_22;
+	private JTextField textField_23;
+	private JTextField textField_10;
+	private JTextField textField_13;
+	private JTextField textField_14;
+	private JTextField textField_24;
+	private JTextField textField_25;
+	private JTextField textField_26;
+	private JTextField textField_27;
 
 	/**
 	 * Launch the application.
@@ -254,6 +269,263 @@ public class MainFrame {
 		);
 		insertBook.setLayout(gl_insertBook);
 		
+		JPanel insertAlbum = new JPanel();
+		frame.getContentPane().add(insertAlbum, "insertAlbum");
+		
+		JButton button = new JButton("Submit");
+		
+		JButton button_1 = new JButton("Cancel");
+		
+		JLabel lblLanguage = new JLabel("language:");
+		
+		textField_16 = new JTextField();
+		textField_16.setColumns(10);
+		
+		JLabel lblAlbumName = new JLabel("Album name:");
+		
+		JLabel lblYear = new JLabel("Year:");
+		
+		JLabel lblMusicName = new JLabel("Music name:");
+		
+		textField_17 = new JTextField();
+		textField_17.setColumns(10);
+		
+		textField_18 = new JTextField();
+		textField_18.setColumns(10);
+		
+		textField_19 = new JTextField();
+		textField_19.setColumns(10);
+		
+		JLabel lblDiskType = new JLabel("Disk type:");
+		
+		JLabel lblProducer = new JLabel("Producer:");
+		
+		JLabel lblSongWriter = new JLabel("Song writer:");
+		
+		JLabel lblComposer = new JLabel("Composer:");
+		
+		JLabel lblArranger = new JLabel("Arranger:");
+		
+		textField_20 = new JTextField();
+		textField_20.setColumns(10);
+		
+		textField_21 = new JTextField();
+		textField_21.setColumns(10);
+		
+		textField_22 = new JTextField();
+		textField_22.setColumns(10);
+		
+		textField_23 = new JTextField();
+		textField_23.setColumns(10);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		GroupLayout gl_insertAlbum = new GroupLayout(insertAlbum);
+		gl_insertAlbum.setHorizontalGroup(
+			gl_insertAlbum.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_insertAlbum.createSequentialGroup()
+					.addGap(173)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_insertAlbum.createSequentialGroup()
+							.addComponent(button, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(button_1))
+						.addGroup(gl_insertAlbum.createSequentialGroup()
+							.addGroup(gl_insertAlbum.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_insertAlbum.createSequentialGroup()
+									.addGroup(gl_insertAlbum.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblYear)
+										.addComponent(lblMusicName)
+										.addComponent(lblLanguage)
+										.addComponent(lblDiskType)
+										.addComponent(lblProducer)
+										.addComponent(lblSongWriter)
+										.addComponent(lblComposer)
+										.addComponent(lblArranger))
+									.addGap(22))
+								.addGroup(gl_insertAlbum.createSequentialGroup()
+									.addComponent(lblAlbumName)
+									.addGap(18)))
+							.addGroup(gl_insertAlbum.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(textField_19, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_20, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_21, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_22, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_23, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_16, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_10, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+								.addComponent(textField_18)
+								.addComponent(textField_17))))
+					.addContainerGap(239, Short.MAX_VALUE))
+		);
+		gl_insertAlbum.setVerticalGroup(
+			gl_insertAlbum.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_insertAlbum.createSequentialGroup()
+					.addContainerGap(21, Short.MAX_VALUE)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_insertAlbum.createSequentialGroup()
+							.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textField_19, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblAlbumName))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textField_18, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblYear))
+							.addGap(18)
+							.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textField_17, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblMusicName))
+							.addGap(18)
+							.addComponent(textField_16, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblLanguage))
+					.addGap(18)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_22, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblDiskType))
+					.addGap(18)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_21, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblProducer))
+					.addGap(18)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_23, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblSongWriter))
+					.addGap(18)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_20, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblComposer))
+					.addGap(18)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_10, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblArranger))
+					.addGap(56)
+					.addGroup(gl_insertAlbum.createParallelGroup(Alignment.BASELINE)
+						.addComponent(button)
+						.addComponent(button_1))
+					.addGap(111))
+		);
+		insertAlbum.setLayout(gl_insertAlbum);
+		
+		JPanel insertMovie = new JPanel();
+		frame.getContentPane().add(insertMovie, "insertMovie");
+		
+		JButton button_2 = new JButton("Submit");
+		
+		JButton button_3 = new JButton("Cancel");
+		
+		JLabel lblReleaseYear = new JLabel("Release year:");
+		
+		JLabel lblMovieName_1 = new JLabel("Movie name:");
+		
+		JLabel lblReleaseYear_1 = new JLabel("Release year:");
+		
+		JLabel lblCrewName = new JLabel("Crew name:");
+		
+		JLabel lblRole = new JLabel("Role:");
+		
+		JLabel lblMovieName = new JLabel("Movie name:");
+		
+		textField_13 = new JTextField();
+		textField_13.setColumns(10);
+		
+		textField_14 = new JTextField();
+		textField_14.setColumns(10);
+		
+		textField_24 = new JTextField();
+		textField_24.setColumns(10);
+		
+		textField_25 = new JTextField();
+		textField_25.setColumns(10);
+		
+		textField_26 = new JTextField();
+		textField_26.setColumns(10);
+		
+		textField_27 = new JTextField();
+		textField_27.setColumns(10);
+		
+		JButton button_4 = new JButton("Submit");
+		
+		JButton button_5 = new JButton("Cancel");
+		GroupLayout gl_insertMovie = new GroupLayout(insertMovie);
+		gl_insertMovie.setHorizontalGroup(
+			gl_insertMovie.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_insertMovie.createSequentialGroup()
+					.addGap(199)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_insertMovie.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED, 248, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(button_3))
+						.addGroup(gl_insertMovie.createSequentialGroup()
+							.addGroup(gl_insertMovie.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_insertMovie.createSequentialGroup()
+									.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_insertMovie.createSequentialGroup()
+									.addGroup(gl_insertMovie.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblMovieName)
+										.addComponent(lblReleaseYear)
+										.addComponent(lblMovieName_1)
+										.addComponent(lblReleaseYear_1)
+										.addComponent(lblCrewName)
+										.addComponent(lblRole))
+									.addGroup(gl_insertMovie.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_insertMovie.createSequentialGroup()
+											.addGap(22)
+											.addGroup(gl_insertMovie.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(textField_26)
+												.addComponent(textField_27, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)))
+										.addGroup(gl_insertMovie.createSequentialGroup()
+											.addGap(18)
+											.addGroup(gl_insertMovie.createParallelGroup(Alignment.LEADING)
+												.addComponent(textField_24, GroupLayout.DEFAULT_SIZE, 302, Short.MAX_VALUE)
+												.addComponent(textField_14, GroupLayout.DEFAULT_SIZE, 302, Short.MAX_VALUE)
+												.addComponent(textField_25, GroupLayout.DEFAULT_SIZE, 302, Short.MAX_VALUE)
+												.addComponent(textField_13, GroupLayout.DEFAULT_SIZE, 302, Short.MAX_VALUE))))))
+							.addGap(9)))
+					.addGap(198))
+		);
+		gl_insertMovie.setVerticalGroup(
+			gl_insertMovie.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_insertMovie.createSequentialGroup()
+					.addGap(69)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_27, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblMovieName))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_26, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblReleaseYear))
+					.addGap(18)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_4)
+						.addComponent(button_5))
+					.addPreferredGap(ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_25, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblMovieName_1))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblReleaseYear_1)
+						.addComponent(textField_24, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(23)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCrewName)
+						.addComponent(textField_14, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblRole)
+						.addComponent(textField_13, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_insertMovie.createParallelGroup(Alignment.BASELINE)
+						.addComponent(button_2)
+						.addComponent(button_3))
+					.addGap(147))
+		);
+		insertMovie.setLayout(gl_insertMovie);
+		
 		
 		
 		
@@ -266,27 +538,32 @@ public class MainFrame {
 		
 		JMenuItem mntmBook = new JMenuItem("Book");
 		mntmBook.addActionListener(new ActionListener() {
-	          public void actionPerformed(ActionEvent event) {
-	        	// go to insert book page
-	        	  CardLayout c = (CardLayout)(frame.getContentPane().getLayout());
-	        	  c.show(frame.getContentPane(), "insertBook");
-	          }
-	        });
-//		mntmBook.addMouseListener(new MouseAdapter() {
-//			@Override
-//			public void mouseClicked(MouseEvent arg0) {
-//
-//	            // go to insert book page
-//	        	  CardLayout c = (CardLayout)(frame.getContentPane().getLayout());
-//	        	  c.show(frame.getContentPane(), "insertBook");
-//			}
-//		});
+          public void actionPerformed(ActionEvent event) {
+        	// go to insert book page
+        	  CardLayout c = (CardLayout)(frame.getContentPane().getLayout());
+        	  c.show(frame.getContentPane(), "insertBook");
+          }
+        });
 		mnInsert.add(mntmBook);
 		
 		JMenuItem mntmAlbum = new JMenuItem("Album");
+		mntmAlbum.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent event) {
+        	// go to insert book page
+        	  CardLayout c = (CardLayout)(frame.getContentPane().getLayout());
+        	  c.show(frame.getContentPane(), "insertAlbum");
+          }
+        });
 		mnInsert.add(mntmAlbum);
 		
 		JMenuItem mntmMovie = new JMenuItem("Movie");
+		mntmMovie.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent event) {
+	        	// go to insert book page
+	        	  CardLayout c = (CardLayout)(frame.getContentPane().getLayout());
+	        	  c.show(frame.getContentPane(), "insertMovie");
+	          }
+	        });
 		mnInsert.add(mntmMovie);
 		
 		JMenu mnUpdate = new JMenu("Update");
